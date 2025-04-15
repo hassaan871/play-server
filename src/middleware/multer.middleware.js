c402e6d518fe629efa72aa00ce182f96e0ad9b2e
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-04-15 19:41:07 +0500

    Multer middleware updated. FileFilter validation applied for both videos and images. Updated the middleware dynamic for both images and videos.

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,14 +1,58 @@
 import multer from "multer";
+import path from "path";
 
+// Base storage configuration
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './punlic/temp')
+
+        // Deteermine destinatio based on file type
+        const destinationPath = file.fieldname.includes('video') ? "./public/temp/videos" : "./public/temp/images";
+        cb(null, destinationPath);
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname)
+        const uniqueName = `${Date.now()}-${file.originalname}`;
+        cb(null, uniqueName);
     }
 });
 
-const upload = multer({ storage });
+// Image file filter
+const imageFileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png/;
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedTypes.test(file.mimetype);
 
-export default upload;
+    if (extname && mimetype) {
+        cb(null, true);
+    } else {
+        cb(new Error("Only images files (jpeg, jpg, png) are allowd"));
+    }
+};
+
+// video file filter
+const videoFileFilter = (req, file, cb) => {
+    // const allowedTypes = /mp4|mov|avi|mkv|webm/
+    const allowedTypes = /mp4/;
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = /video\/.*/.test(file.mimetype);
+
+    if (extname && mimetype) {
+        cb(null, true);
+    } else {
+        // cb(new Error("Only video files (mp4, mov, avi, mkv, webm) are allowed"));
+        cb(new Error("Only video files (mp4) are allowed"));
+    }
+}
+
+const uploadImage = multer({
+    storage,
+    fileFilter: imageFileFilter,
+    limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
+});
+
+const uploadVideo = multer({
+    storage,
+    fileFilter: videoFileFilter,
+    limits: { fileSize: 500 * 1024 * 1024 } // 500MB limit
+});
+
+export { uploadImage, uploadVideo };
