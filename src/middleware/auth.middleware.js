cb33828fa2476bd2cd96463aa9ea34206b373ec6
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-04-15 13:02:34 +0500

    auth middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,22 @@
+import tokenServices from "../services/jwt.service.js";
+
+const auth = (req, res, next) => {
+    try {
+        const token = req.header("x-auth-token");
+        if(!token) return res.status(401).json({
+            success: false,
+            messsage: "Access denied. No token provided"
+        });
+
+        const decoded = tokenServices.verifyToken(token);
+        req.user = {userId: decoded._id};
+        next();
+
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            messsage: "Auth error, authentication Failed!!!",
+            error
+        });
+    }
+}
