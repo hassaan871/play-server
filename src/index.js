f13ea42e1bbbdc241928ecdf5f6df8b3a1fd7169
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-04-16 14:40:24 +0500

    used cron job utilties

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ dotenv.config({
 
 import connectDB from "./db/dbConnect.js";
 import { app } from "./app.js";
+import { scheduleCleanupJob, runImmediateCleanup } from "../src/util/cleanup.utility.js";
 
 connectDB()
     .then(() => {
+
+        // Schedule cleanup job for unverified users
+        scheduleCleanupJob();
+
+        // Run an immediate cleanup on startup
+        runImmediateCleanup();
+
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is running at PORT: ${process.env.PORT}`);
         })
