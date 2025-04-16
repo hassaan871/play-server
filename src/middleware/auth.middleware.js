d93b5b1dfafce01914388a73147a379c2811a105
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-04-16 14:42:29 +0500

    quering user from db for secure authentication

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,7 @@
+import User from "../models/user.model.js";
 import tokenServices from "../services/jwt.service.js";
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
     try {
         const token = req.header("x-auth-token");
         if (!token) return res.status(401).json({
@@ -9,7 +10,20 @@ const auth = (req, res, next) => {
         });
 
         const decoded = tokenServices.verifyToken(token);
-        req.user = { userId: decoded._id };
+        
+        const user = await User.findById(decoded._id).select("-password");
+        if(!user) return res.status(401).json({
+            success: false,
+            message: "Invalid token. User not found." 
+        });
+
+        if(!user.isVerified) return res.status(403).json({
+            success: false,
+            message: "Email not verified. Please verify your email to access this resource."
+        });
+
+        req.user = { user };
+
         next();
 
     } catch (error) {
