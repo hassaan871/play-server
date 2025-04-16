482d5d8cec9917dd800a21c8d83a6b340d525a34
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-04-16 14:42:52 +0500

    user email validation

--- src/validations/validations.js
+++ src/validations/validations.js
@@ -1,14 +1,20 @@
 import Joi from "joi";
 
+const eamilSchema = Joi.string().email().required();
+
 const passwordSchema = Joi.string().min(5).required();
 
 const validateUserData = {
 
     Signup: Joi.object({
         username: Joi.string().min(3).required(),
-        email: Joi.string().email().required(),
+        email: eamilSchema,
         fullName: Joi.string().min(5).required(),
         password: passwordSchema
+    }),
+
+    EamilVerification: Joi.object({
+        email: eamilSchema
     }),
 
     ResetPassword: Joi.object({
