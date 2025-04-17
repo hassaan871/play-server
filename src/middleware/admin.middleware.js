171a37e941b6cfa04159397b87904ca068c77710
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-04-17 12:04:42 +0500

    feat(admin): add isAdmin middleware to enforce admin access

---
+++ src/middleware/admin.middleware.js
@@ -0,0 +1,23 @@
+const isAdmin = async (req, res, next) => {
+    try {
+        if(!req.user) return res.status(401).json({
+            success: false,
+            message: "Authentication required"
+        });
+
+        if(!req.user.isAdmin) return res.status.json({
+            success: false,
+            message: "Admin access required. Unauthorized access"
+        });
+        
+        next();
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Admin authorization failed",
+            error
+        });
+    }
+};
+
+export default isAdmin;
