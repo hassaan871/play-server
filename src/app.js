e181780120a330fc56f1fd7493bd8a7be7fb360d
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-04-23 12:56:05 +0500

    cookie parser configured

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes/routes.js";
 import path from "path";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors({
 app.set("view engine", "ejs");
 app.set("views", path.join(path.resolve(), "src/views"));
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
