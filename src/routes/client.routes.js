a308c45f64011d5d4c36e1531cf2bb3cfce42fc2
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-04-23 15:41:42 +0500

    homepage client route protencted

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const routes = express.Router();
 
+import clientAuth from "../middleware/clientAuth.middleware.js";
+
 routes.get('/login', (req, res) => res.render('login'));
 routes.get('/signup', (req, res) => res.render('signup'));
-routes.get('/home', (req, res) => res.render('home'));
+routes.get('/home', [clientAuth],(req, res) => res.render('home'));
 
 export default routes;
